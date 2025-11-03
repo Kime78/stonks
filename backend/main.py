28ee6ce4b8d38855e2875e4149a0a6d5dcba9814
Mihalache Mihai <kime78.dev@gmail.com>
2025-11-03 12:08:51 +0200

    Create basic API call

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,11 @@
-print('first')
+import httpx
+from dotenv import load_dotenv  
+import os
+from fastapi import FastAPI
+load_dotenv()
+
+app = FastAPI()
+
+@app.get('/intraday')
+def get_intraday_data():
+    return  httpx.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={os.getenv("ALPHA_VANTAGE_API")}").json()
